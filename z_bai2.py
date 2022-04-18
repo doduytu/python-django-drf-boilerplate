4349b48a789df350c16b47585bee18a6d8e9ce93
TuDV <tudv@hybrid-technologies.vn>
2022-04-18 14:11:00 +0700

    ok

--- z_bai2.py
+++ z_bai2.py
@@ -1,1 +1,10 @@
 print("bài 2: Viết chương trình nhập vào ngày, tháng, năm. tính tổng số ngày từ đầu năm cho tới ngày nhập vào.")
+
+print("Nhap Ngay: ngay = ", end=" ")
+ngay = int(input())
+print("Nhap Thang: thang = ", end=" ")
+thang = int(input())
+print("Nhap Nam: nam = ", end=" ")
+nam = int(input())
+
+print("Thoi gian da nhap: " + str(nam) + "/" + str(thang) + "/" + str(ngay))
